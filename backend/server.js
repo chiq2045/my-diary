0e9dc30671b936e2d314f8f680fa48ab0957c1c6
Chiq2045 <Charles.Oroko@outlook.com>
2020-06-23 23:16:06 -0700

    complete user routes
    use mount to add user routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const Koa = require('koa');
 const logger = require('koa-logger');
+const mount = require('koa-mount');
+const { resolve } = require('path');
 
 const server = new Koa();
-
 const PORT = '1337';
 server.use(logger());
+
+const users = require(resolve(__dirname, 'users'));
+server.use(mount('/users', users));
 
 server.listen(PORT, () => {
   console.log('The server is running at http://localhost:' + PORT);
--- backend/users/index.js
+++ backend/users/index.js
@@ -1,13 +1,50 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
+const koaBody = require('koa-body');
 
 const users = new Koa();
 const userRouter = new Router();
 
-userRouter.get('/', () => {
-  console.log('Get all users');
-  // TODO: implement users route
-});
+userRouter
+  .get('/', ctx => {
+    const { id } = ctx.query;
+    if (!id) {
+      console.log('Get all users');
+      ctx.body = { message: 'There are no users yet' };
+    } else{
+      console.log(`Get user with id ${id}`);
+      ctx.body = { id: id, message: 'Still no user here yet' };
+    }
+    ctx.status = 200;
+  })
+  .post('/', koaBody(), ctx => {
+    const { userName, password } = ctx.request.body;
+    console.log('Add the following user:');
+    console.log({
+      userName: userName,
+      password: password
+    });
+    ctx.status = 200;
+    ctx.body = {
+      message: 'User creation is not yet implemented',
+      user: {
+        userName: userName,
+        password: password
+      }
+    };
+  })
+  .put('/', koaBody(), ctx => {
+    const { id } = ctx.query;
+    console.log(`Update user with id ${id}`);
+    ctx.status = 200;
+    ctx.body = { id: id, message: 'Still no user here yet' };
+  })
+  .delete('/', koaBody(), ctx => {
+    const { id } = ctx.query;
+    console.log(`Delete user with id ${id}`);
+    ctx.status = 200;
+    ctx.body = { id: id, message: 'Still no user to delete here here yet' };
+  });
 
 users.use(userRouter.routes());
 users.use(userRouter.allowedMethods());
